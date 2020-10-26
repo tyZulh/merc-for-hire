cd8a333151767d8ed0419cd155a494d3598e07ae
TyZulh <thomas.paoli.tp@gmail.com>
2020-10-26 22:16:14 +0100

    feat : api call to fetch list of spells

--- src/components/ListSpells.js
+++ src/components/ListSpells.js
@@ -1,6 +1,15 @@
 import React from 'react'
+import axios from 'axios'
+
+const baseUrl = 'https://www.dnd5eapi.co'
 
 class ListSpells extends React.Component {
+
+  fetchData = async () => {
+    const resp = await axios.get(`${baseUrl}/api/spells`)
+    this.setState({spells : resp.data.result})
+  }
+
   render() {
     return (
       <div className='listSpells'>
