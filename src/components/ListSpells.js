d65321f36adb25b4a97ad690a7cab6a6804c2dfc
TyZulh <thomas.paoli.tp@gmail.com>
2020-10-26 22:08:04 +0100

    feat : create ListSpells component

---
+++ src/components/ListSpells.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class ListSpells extends React.Component {
+  render() {
+    return (
+      <div className='listSpells'>
+
+      </div>
+    )
+  }
+}
+
+export default ListSpells
