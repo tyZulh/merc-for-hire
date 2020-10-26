68934df535098a7638ca5cfb931be3bc02a4aac1
TyZulh <thomas.paoli.tp@gmail.com>
2020-10-26 22:44:14 +0100

    feat : display the spellcard components for each spell

--- src/components/ListSpells.js
+++ src/components/ListSpells.js
@@ -1,24 +1,32 @@
 import React from 'react'
 import axios from 'axios'
+import SpellCard from './SpellCard'
 
 const baseUrl = 'https://www.dnd5eapi.co'
 
 class ListSpells extends React.Component {
 
-  
+  state = {
+    spells : []
+  }
+
   componentDidMount() {
     this.fetchData()
   }
 
   fetchData = async () => {
     const resp = await axios.get(`${baseUrl}/api/spells`)
-    this.setState({spells : resp.data.result})
+    this.setState({spells : resp.data.results})
   }
 
   render() {
     return (
       <div className='listSpells'>
-
+        {this.state.spells && this.state.spells.map(spell => {
+          return(
+            <SpellCard url = {spell.url} />
+          )
+        })}
       </div>
     )
   }
