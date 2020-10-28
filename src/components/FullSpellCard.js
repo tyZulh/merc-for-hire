4de2eec8bb1f3f316b938f4433e088d87c0969cc
Thomas Paoli <thomas.paoli.tp@gmail.com>
2020-10-28 22:15:19 +0100

    feat : WIP

--- src/components/FullSpellCard.js
+++ src/components/FullSpellCard.js
@@ -3,25 +3,46 @@
 const FullSpellCard = ({spell}) => {
   return(
     <div className='fullSpellCard' >
-      <h2>{spell.name}</h2>
-      <p>Casting time :</p>
-      <p>{spell.casting_time}</p>
-      <p>Duration</p>
-      <p>{spell.duration}</p>
-      <p>Range</p>
-      <p>{spell.range}</p>
-      <p>Material</p>
-      <p>{spell.material}</p>
-      <p>Component</p>
-      <p>{spell.components.map(text => {
-        return `${text} `
-      })}</p>
-      <p>Description</p>
-      <p>{spell.desc.map(text => {
-        return `${text} `
-      })}</p>
-      <p>School</p>
-      <p>{spell.school.name}</p>
+      <h2 className='fullSpellCard-title'>{spell.name}</h2>
+      
+      <div className='fullSpellCard-info'>
+        <p className='fullSpellCard-info-title'>Casting time : </p>
+        <p className=''>{spell.casting_time}</p>
+      </div>
+
+      <div className='fullSpellCard-info'>
+        <p className='fullSpellCard-info-title'>Duration : </p>
+        <p className=''>{spell.duration}</p>
+      </div>
+
+      <div className='fullSpellCard-info'>
+        <p className='fullSpellCard-info-title'>Range :</p>
+        <p className=''>{spell.range}</p>
+      </div>
+
+      <div className='fullSpellCard-info'>
+        <p className='fullSpellCard-info-title'>Material : </p>
+        <p className=''>{spell.material}</p>
+      </div>
+
+      <div className='fullSpellCard-info'>
+        <p className='fullSpellCard-info-title'>Component : </p>
+        <p className=''>{spell.components.map(text => {
+          return `${text} `
+        })}</p>
+      </div>
+
+      <div className='fullSpellCard-info'>
+        <p className='fullSpellCard-info-title'>Description : </p>
+        <p className='fullSpellCard-info-text'>{spell.desc.map(text => {
+          return `${text} `
+        })}</p>
+      </div>
+
+      <div className='fullSpellCard-info'>
+        <p className='fullSpellCard-info-title'>School : </p>
+        <p className=''>{spell.school.name}</p>
+      </div>
     </div>
   )
 }
